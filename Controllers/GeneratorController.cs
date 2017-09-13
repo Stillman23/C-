00eb6178786e73f5e9f50fe66779177b689e2fc9
Scott Tillman <Scotttillman23@gmail.com>
2017-09-13 15:58:14 -0500

    passcode

---
+++ Controllers/GeneratorController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace passcodeGenerator.Controllers
+{
+    public class GeneratorController : Controller
+    {
+
+        public string genPasscode()
+        {
+            string digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random randNum = new Random();
+            char[] passcodeArr = new char[14];
+            for (int i = 0; i < passcodeArr.Length; i++){
+                passcodeArr[i] = digits[randNum.Next(digits.Length)];
+            }
+
+            string passcode = new String(passcodeArr);
+            return passcode;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+
+            int? counter = HttpContext.Session.GetInt32("counter");
+            if (counter == null)
+             {
+                counter = 1;
+            } else {
+                counter += 1;
+            }
+
+            HttpContext.Session.SetInt32("counter", (int)counter);
+
+            ViewBag.counter = counter;
+            ViewBag.passcode = genPasscode();
+            
+            return View("index");
+        }
+            
+       [HttpGet]
+        [Route("generate")]
+        public IActionResult generate()
+        {
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        [Route("reset")]
+        public IActionResult reset()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("index");
+        }
+    }
+}
